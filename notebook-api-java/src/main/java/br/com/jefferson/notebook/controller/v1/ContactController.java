48eb2d51ac897267d4051504f2762e6ff69a83da
jeffersonacm <jeffersonacm@hotmail.com>
2020-01-20 23:46:08 -0300

    Add post, put and delete contact

--- notebook-api-java/src/main/java/br/com/jefferson/notebook/controller/v1/ContactController.java
+++ notebook-api-java/src/main/java/br/com/jefferson/notebook/controller/v1/ContactController.java
@@ -3,9 +3,12 @@ package br.com.jefferson.notebook.controller.v1;
 import br.com.jefferson.notebook.domain.model.Contact;
 import br.com.jefferson.notebook.domain.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +38,42 @@ public class ContactController {
             return ResponseEntity.ok(contact.get());
         } else {
             return ResponseEntity.noContent().build();
+        }
+    }
+
+
+    @PostMapping
+    private ResponseEntity save(@RequestBody Contact contact) {
+        Contact contactSaved = contactRepository.save(contact);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(contactSaved.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping
+    private ResponseEntity update(@RequestBody Contact contact) {
+        Optional<Contact> contact1 = contactRepository.findById(contact.getId());
+        if(contact1.isPresent()) {
+            contactRepository.save(contact);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(value = "{id}")
+    private ResponseEntity delete(@PathVariable String id) {
+        Optional<Contact> contact = contactRepository.findById(id);
+
+        if(contact.isPresent()) {
+            contactRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 
